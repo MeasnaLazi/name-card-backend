adee3bf0b2a1353751d461338dc041d54c9df5be
sna_lodosha <measna.ly@lodosha.jp>
2023-10-19 21:03:43 -0500

    modify user_service.test.js to able to run test

--- test/service/user_service.test.js
+++ test/service/user_service.test.js
@@ -1,30 +1,53 @@
 const { expect }= require("chai");
 
 let userService;
+let USER = {
+    email: "test@test.com",
+    username: "test",
+    password: "test123!",
+    invalid_password: "wrongPassword",
+}
 
 describe("[UserService]", async () => {
 
     before(() => { 
-            userService = require("../../src/services/UserService");
-        });
+        userService = require("../../src/services/UserService");
+    });
     
-    it("Create new user", () => {
+    it("Create new user", (done) => {
         userService
-            .createUser("test@test.com", "test", "test123!")
+            .createUser(USER.email, USER.username, USER.password)
             .then(data => {
                 expect(data).to.be.not.equal(null);
-            });
+                done();
+            }).catch(done);
     });
 
-    it("Check if user exist", () => {
+    it("Check if user exist", (done) => {
         userService
             .isUserExist()
             .then(isExist => {
                 expect(isExist).to.be.equal(true);
-            });
+                done();
+            }).catch(done);
     });
 
-    // after(() => {
-    //     closeDatabase();
-    // })
+    it("Login successful", (done) => {
+        userService
+            .login(USER.username, USER.password)
+            .then(user => {
+                expect(user).to.be.not.equal(null);
+                done();
+            }).catch(done);
+    });
+
+    it("Login unsuccessful should throw error", () => {
+        return userService
+            .login(USER.username, USER.invalid_password)
+            .then(user => {
+                expect(user).to.be.equal(null);
+            }).catch(err => {
+                expect(err).to.be.not.equal(null);
+            });
+    });
 });
